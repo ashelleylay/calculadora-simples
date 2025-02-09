3d938466e3224950a72b6e368dc3fa06c2e63fcd
ashelleylay <ashelleylay11a@gmail.com>
2025-02-09 19:49:01 -0300

    ajuste final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,49 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'calculadora';
+  display: string = '0';
+  currentOperator: string = '';
+  previousValue: number = 0;
+
+  appendToDisplay(value: string) {
+    if (this.display === '0') {
+      this.display = value;
+    } else {
+      this.display += value;
+    }
+  }
+
+  clearDisplay() {
+    this.display = '0';
+  }
+
+  setOperator(operator: string) {
+    this.currentOperator = operator;
+    this.previousValue = parseFloat(this.display);
+    this.display = '0';
+  }
+
+  calculate() {
+    const currentValue = parseFloat(this.display);
+    let result: number = 0;
+
+    switch (this.currentOperator) {
+      case '+':
+        result = this.previousValue + currentValue;
+        break;
+      case '-':
+        result = this.previousValue - currentValue;
+        break;
+      case '*':
+        result = this.previousValue * currentValue;
+        break;
+      case '/':
+        result = this.previousValue / currentValue;
+        break;
+    }
+
+    this.display = result.toString();
+    this.currentOperator = '';
+    this.previousValue = 0;
+  }
 }
